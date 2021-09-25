e0b1840cad37bbd468a732efa399465c6868d16b
zqllz <740402045@qq.com>
2021-09-25 17:16:28 +0800

    【add】538

---
+++ leetcode/lc538/lc538.java
@@ -0,0 +1,25 @@
+package lc538;
+
+public class lc538 {
+
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int [][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                char c1 = word1.charAt(i - 1);
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int lcs = dp[m][n];
+        return m - lcs + n - lcs;
+    }
+
+
+}
